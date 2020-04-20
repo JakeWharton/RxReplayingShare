592a93e3cec44243c06bf873ffd6a9004fc8676f
Zac Sweers <zsweers@slack-corp.com>
2020-04-20 16:21:41 -0400

    Move to com.jakewharton.rx3 package

--- replaying-share/src/main/java/com/jakewharton/rx3/ReplayingShare.java
+++ replaying-share/src/main/java/com/jakewharton/rx3/ReplayingShare.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jakewharton.rx;
+package com.jakewharton.rx3;
 
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.FlowableTransformer;
